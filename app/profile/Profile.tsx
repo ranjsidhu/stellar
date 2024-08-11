a306ea90c77c282869b59c749a367f7f8334865a
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-08-11 09:34:26 +0100

    feat: start on profile UI

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -1,25 +1,34 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import { User } from "../types";
+import "./profile.css";
 
 export default function Profile() {
+  const router = useRouter();
   const [details, setDetails] = useState<User | null>(null);
 
   useEffect(() => {
     const user = localStorage.getItem("user")!;
     const userDetails: User = JSON.parse(user);
+    if (!userDetails.id) {
+      router.push("/login");
+    }
     setDetails(userDetails);
-  }, []);
+  }, [router]);
 
   return (
-    <>
+    <div className="profile-wrapper">
+      <Avatar size={64} icon={<UserOutlined />} />
       {details &&
         Object.entries(details).map(([key, value]) => (
           <div key={key}>
             {key}: {value?.toString()}
           </div>
         ))}
-    </>
+    </div>
   );
 }
