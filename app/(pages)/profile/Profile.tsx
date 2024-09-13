51481b243ab3b10b1cd6ae0a592b1ab7dcbd7d67
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-09-13 15:08:39 +0100

    chore: remove profile temporarily

--- app/(pages)/profile/Profile.tsx
+++ app/(pages)/profile/Profile.tsx
@@ -1,33 +1,33 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { getItem } from "@/app/utils/storage";
-import type { User } from "@/app/types";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { getItem } from "@/app/utils/storage";
+// import type { User } from "@/app/types";
 import styles from "./Profile.module.css";
 
 export default function Profile() {
-  const router = useRouter();
-  const [details, setDetails] = useState<User | null>(null);
+  // const router = useRouter();
+  // const [details, setDetails] = useState<User | null>(null);
 
-  useEffect(() => {
-    const userDetails = getItem("userDetails");
-    const parsedUserDetails: User | null = userDetails ? userDetails : null;
-    if (!parsedUserDetails?.id || parsedUserDetails?.id === -1) {
-      router.push("/login?return=profile");
-      return;
-    }
-    setDetails(parsedUserDetails);
-  }, [router]);
+  // useEffect(() => {
+  //   const userDetails = getItem("userDetails");
+  //   const parsedUserDetails: User | null = userDetails ? userDetails : null;
+  //   if (!parsedUserDetails?.id || parsedUserDetails?.id === -1) {
+  //     router.push("/login?return=profile");
+  //     return;
+  //   }
+  //   setDetails(parsedUserDetails);
+  // }, [router]);
 
   return (
     <div className={styles.profileWrapper}>
-      {details &&
+      {/* {details &&
         Object.entries(details).map(([key, value]) => (
           <div key={key}>
             {key}: {value?.toString()}
           </div>
-        ))}
+        ))} */}
     </div>
   );
 }
