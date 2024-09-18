28a874ac0be3db5512877dadde01bacfd32036ef
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-09-18 09:35:55 +0100

    feat: initial profile page

--- app/(pages)/profile/Profile.tsx
+++ app/(pages)/profile/Profile.tsx
@@ -1,33 +1,46 @@
 "use client";
 
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import { getItem } from "@/app/utils/storage";
-// import type { User } from "@/app/types";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getItem } from "@/app/utils/storage";
+import type { User } from "@/app/types";
 import styles from "./Profile.module.css";
 
 export default function Profile() {
-  // const router = useRouter();
-  // const [details, setDetails] = useState<User | null>(null);
+  const router = useRouter();
+  const [details, setDetails] = useState<User | null>(null);
+  const [role, setRole] = useState<String>("");
 
-  // useEffect(() => {
-  //   const userDetails = getItem("userDetails");
-  //   const parsedUserDetails: User | null = userDetails ? userDetails : null;
-  //   if (!parsedUserDetails?.id || parsedUserDetails?.id === -1) {
-  //     router.push("/login?return=profile");
-  //     return;
-  //   }
-  //   setDetails(parsedUserDetails);
-  // }, [router]);
+  useEffect(() => {
+    const userDetails = getItem("userDetails");
+    const parsedUserDetails: User | null = userDetails ? userDetails : null;
+    const invalidDetails =
+      !parsedUserDetails?.id ||
+      parsedUserDetails?.id === -1 ||
+      !parsedUserDetails?.roles;
+    if (invalidDetails) {
+      router.push("/login?return=profile");
+      return;
+    }
+    setDetails(parsedUserDetails);
+    setRole(parsedUserDetails.roles.name);
+  }, [router]);
 
   return (
     <div className={styles.profileWrapper}>
-      {/* {details &&
-        Object.entries(details).map(([key, value]) => (
-          <div key={key}>
-            {key}: {value?.toString()}
-          </div>
-        ))} */}
+      {!details && <div>Loading...</div>}
+      {details && (
+        <>
+          {Object.entries(details)
+            .filter((e) => e[0] !== "roles")
+            .map(([key, value]) => (
+              <div key={key}>
+                {key}: {value?.toString()}
+              </div>
+            ))}
+          <div>Role: {role}</div>
+        </>
+      )}
     </div>
   );
 }
