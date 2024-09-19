8e913bcfe540becc9df47b9f97885b8fb34c6b7a
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-09-19 14:53:55 +0100

    feat: download file from bucket

--- app/api/(storage)/bucket/cv/[id]/route.ts
+++ app/api/(storage)/bucket/cv/[id]/route.ts
@@ -1,19 +1,42 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/app/utils/supabase/server";
 
+const { NEXT_PUBLIC_CV_BUCKET } = process.env;
+
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
-    //   TODO - cross reference db table with id in params to get file by name - need filetype in the download param to get the file
     const { id } = params;
+    if (!id) throw new Error("No file id provided");
     const supabase = createClient();
-    const { data } = await supabase.storage
-      .from("stellar-cv-docs")
+    const { data, error } = await supabase.storage
+      .from(NEXT_PUBLIC_CV_BUCKET!)
       .download(id);
 
-    return NextResponse.json({ data });
+    if (error) throw new Error(error.message);
+
+    const fileBuffer = await data?.arrayBuffer();
+    const contentType = data?.type || "application/octet-stream";
+
+    const { data: userDocsData, error: userDocsError } = await supabase
+      .from("user_documents")
+      .select("filename")
+      .eq("file_id", id);
+
+    if (userDocsError) throw new Error(userDocsError.message);
+
+    const filename = userDocsData?.[0]?.filename || id;
+
+    return new NextResponse(fileBuffer, {
+      headers: {
+        "Content-Type": contentType,
+        "Content-Disposition": `attachment; filename="${id}"`,
+        "Content-Length": fileBuffer!.byteLength.toString(),
+        "X-Filename": filename,
+      },
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message });
   }
