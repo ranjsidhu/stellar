6e957faae24d921de0e1c7d1acb6024b91b6c7c9
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-10-05 16:11:47 +0100

    feat: initial admin users panel

---
+++ app/api/(auth)/auth/roles/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { client } from "../../../utils/db-client";
+
+export default async function GET() {
+  try {
+    const { data, error } = await client.from("roles").select("*");
+    if (error) throw new Error(error.message);
+
+    return NextResponse.json({
+      response: data,
+      message: "Successfully fetched roles",
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message });
+  }
+}
