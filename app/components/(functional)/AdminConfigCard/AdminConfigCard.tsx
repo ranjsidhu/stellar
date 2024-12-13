eca7b7817ea2b99934214c479fd2dedfb1d22147
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-12-13 12:58:20 +0000

    feat: placeholder delete functionality

--- app/components/(functional)/AdminConfigCard/AdminConfigCard.tsx
+++ app/components/(functional)/AdminConfigCard/AdminConfigCard.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Card } from "antd";
+import { Card, Modal } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { notify } from "@/app/components";
 import type { AdminCardProps } from "@/app/types";
 
 const { Meta } = Card;
@@ -12,21 +14,46 @@ export default function AdminConfigCard({
   description,
   route,
 }: AdminCardProps) {
+  const [modalOpen, setModalOpen] = useState(false);
   const router = useRouter();
 
   const actions: React.ReactNode[] = [
     <EditOutlined
       key="edit"
-      onClick={() => {
-        router.push(route);
-      }}
+      style={{ color: "#00150f" }}
+      onClick={() => router.push(`/admin/configuration/edit${route}`)}
     />,
-    <DeleteOutlined key="delete" />,
+    <DeleteOutlined
+      key="delete"
+      style={{ color: "red" }}
+      // TODO - implement this once antd have resolved react 19 compliance
+      // onClick={() => setModalOpen(true)}
+      onClick={() =>
+        notify("info", "Info", "Delete functionality is not available")
+      }
+    />,
   ];
+
+  const DeleteModal = () => {
+    return (
+      <Modal
+        centered
+        title="Are you sure?"
+        okType="danger"
+        okText="Yes"
+        cancelText="No"
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      >
+        <p>Do you want to delete this configuration?</p>
+      </Modal>
+    );
+  };
 
   return (
     <Card style={{ width: 240, height: 150 }} actions={actions}>
       <Meta title={title} description={description} />
+      <DeleteModal />
     </Card>
   );
 }
