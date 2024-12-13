1c7319d24eae5425f39457ad9f851b321b73ec83
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-12-13 20:48:21 +0000

    feat: config add

--- app/(pages)/(protected)/admin/configuration/add/[table]/AdminConfigAdd.tsx
+++ app/(pages)/(protected)/admin/configuration/add/[table]/AdminConfigAdd.tsx
@@ -1,3 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Form, Input, Button, type FormProps } from "antd";
+import { PageLayout, SectionLoading, notify } from "@/app/components";
+import { BasicTable } from "@/app/types";
+import styles from "./AdminConfigAdd.module.css";
+
 export default function AdminConfigAdd({ table }: { table: string }) {
-  return <p>{table}</p>;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [form] = Form.useForm();
+  const { Item } = Form;
+  const router = useRouter();
+
+  const handleSubmit: FormProps<BasicTable>["onFinish"] = (values) => {
+    try {
+      setLoading(true);
+      fetch(`/api/${table}`, {
+        method: "POST",
+        body: JSON.stringify(values),
+      })
+        .then(() => notify("success", "Success", "Record created successfully"))
+        .catch((error: any) => notify("error", "Error", error.message))
+        .finally(() => {
+          setLoading(false);
+          router.back();
+        });
+    } catch (error: any) {
+      notify("error", "Error", error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <PageLayout>
+      <SectionLoading loading={loading}>
+        <Form
+          form={form}
+          layout="vertical"
+          scrollToFirstError
+          onFinish={handleSubmit}
+        >
+          <Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please enter a value" }]}
+          >
+            <Input placeholder="Name of record" disabled={loading} />
+          </Item>
+          <div className={styles.flexButtons}>
+            <Item>
+              <Button type="primary" htmlType="submit" disabled={loading}>
+                Save
+              </Button>
+            </Item>
+            <Item>
+              <Button disabled={loading} onClick={() => router.back()}>
+                Cancel
+              </Button>
+            </Item>
+          </div>
+        </Form>
+      </SectionLoading>
+    </PageLayout>
+  );
 }
