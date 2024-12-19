32b0cca7dfe261091964b239cd6617e413a25c10
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-12-19 17:38:46 +0000

    feat: integrate tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}", // Scans everything under the `app` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
