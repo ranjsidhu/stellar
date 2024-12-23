edd543b4ff75f6a736244302cc186eb5efa70077
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-12-23 14:29:03 +0000

    refactor: redirect into server action

--- app/components/(misc)/Redirect.tsx
+++ app/components/(misc)/Redirect.tsx
@@ -7,6 +7,8 @@ import { createClient } from "@/app/utils/supabase/client";
 import { setSession } from "@/app/redux/features/Auth";
 import { getUserEmail, setItem, getUserRole } from "@/app/utils/storage";
 import { ADMIN_CARDS } from "@/app/constants/admin";
+import { notify } from "../(display)/Notification/Notification";
+import { updateDetails } from "./serveractions";
 
 export default function Redirect() {
   const supabase = createClient();
@@ -15,57 +17,60 @@ export default function Redirect() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const validateSession = async () => {
+    const checkAndUpdateRole = async (email: string, previousRole: string) => {
+      const userdetails = await updateDetails(email);
+      if (!userdetails.success || !userdetails.data) return null;
+
+      const { data } = userdetails;
+      setItem("userDetails", data);
+      const newRole = data.roles.name;
+
+      if (newRole !== previousRole) {
+        window.location.reload();
+      }
+      return newRole || previousRole;
+    };
+
+    const handleRouting = (session: any, role: string | null) => {
       const adminRoutes = [...ADMIN_CARDS.map((card) => card.route), "/admin"];
-      const previousRole = getUserRole();
-      let role = "";
+      const isAuthPage = pathname === "/login" || pathname === "/register";
+      const isAdminRoute = pathname.includes("/admin");
+
+      if (isAuthPage && session) {
+        router.push("/");
+        return;
+      }
+
+      if (
+        (isAdminRoute || adminRoutes.includes(pathname)) &&
+        role !== "Admin"
+      ) {
+        router.push("/");
+      }
+    };
+
+    const validateSession = async () => {
       const email = getUserEmail();
-      if (email) {
-        fetch("/api/auth/roles/refresh", {
-          method: "POST",
-          body: JSON.stringify({ email }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            setItem("userDetails", data);
-            role = data.roles.name;
-            if (role !== previousRole) {
-              window.location.reload();
-            } else {
-              role = previousRole;
-            }
-          })
-          .finally(async () => {
-            const { data } = await supabase.auth.getSession();
-            const { session } = data;
-            dispatch(setSession(session));
+      if (!email) return;
 
-            if (
-              (pathname === "/login" || pathname === "/register") &&
-              session
-            ) {
-              router.push("/");
-              return;
-            }
-
-            if (adminRoutes.includes(pathname) && role !== "Admin") {
-              router.push("/");
-            }
-          });
+      try {
+        const role = await checkAndUpdateRole(email, getUserRole());
+        const { data } = await supabase.auth.getSession();
+        dispatch(setSession(data.session));
+        handleRouting(data.session, role);
+      } catch (error: any) {
+        notify("error", "Error", "Session validation failed:" + error);
       }
     };
 
     validateSession();
 
-    const { data: listener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        dispatch(setSession(session));
-      }
-    );
+    const { data: listener } = supabase.auth.onAuthStateChange((_, session) => {
+      dispatch(setSession(session));
+    });
 
-    return () => {
-      listener?.subscription.unsubscribe();
-    };
+    return () => listener?.subscription.unsubscribe();
   }, [router, pathname, dispatch, supabase.auth]);
+
   return <></>;
 }
