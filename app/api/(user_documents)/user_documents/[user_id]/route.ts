79f303fe865432e26bc233b45326b60a747c165c
Ranjeet Sidhu <ranjsidhudev@gmail.com>
2025-06-25 06:56:04 +0100

    chore: user documents rbac

--- app/api/(user_documents)/user_documents/[user_id]/route.ts
+++ app/api/(user_documents)/user_documents/[user_id]/route.ts
@@ -1,15 +1,32 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/app/api/utils/prisma-utils";
+import { checkValidSession } from "@/app/utils/auth";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ user_id: string }> }
 ) {
   try {
+    const res = await checkValidSession();
+    if (!res.isAuthorized) {
+      return res;
+    }
     const { user_id } = await params;
     if (!user_id) {
       throw new Error("The user_id is undefined");
     }
+    if (user_id !== res.id) {
+      return NextResponse.json(
+        {
+          isAuthorized: false,
+          response: {
+            error: "Unauthorized: User ID does not match session",
+          },
+        },
+        { status: 401 }
+      );
+    }
+
     const userDocuments = await prisma.user_documents.findMany({
       where: { user_id: Number(user_id) },
       include: { file_types: true },
